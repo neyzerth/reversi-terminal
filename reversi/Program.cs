c6f5542a8bfb9e44eb0af2b354c9d1bc96c9920b
neyzer <neyzertoledo@gmail.com>
2025-02-12 22:54:17 -0800

    fixing bugs
    Co-authored-by: POLOK8039 <POLOK8039@users.noreply.github.com>
    Co-authored-by: WearyBerry <WearyBerry@users.noreply.github.com>

--- reversi/Program.cs
+++ reversi/Program.cs
@@ -10,12 +10,10 @@
 
 namespace reversi;
 class Program {
-    static void Main(String[] args) {
-        Console.Clear();
+    static void Main(String[] args) 
+    {
 
-        /*
-            Si les esto ney, falta agregar una que otra validdacion
-        */
+
 
         ConsoleColor[] colors = [
             ConsoleColor.Red,
@@ -30,17 +28,46 @@ class Program {
         string[] nombres = new string[2];
         string[,] tablero = new string[8, 8];
         
-        // for (int i = 0; i < 2; i++) {
-        //     Console.Write($"Ingresa el nombre del jugador {i + 1}: ");
-        //     nombres[i] = Console.ReadLine();
-        //     Console.WriteLine($"{nombres[i]}, selecciona un color: ");
-        //     PrintColors(colors);
-        //     Console.Write("> ");
-        //     colorJugadores[i] = colors[Int32.Parse(Console.ReadLine()) -1];
-        // }
+        for (int i = 0; i < 2; i++) {
+            String error = "Ingresa un valor valido";
+            Console.Clear();
 
-        nombres = ["pepe", "tilin"];
-        colorJugadores = [colors[0], colors[1]];
+            Console.Write($"Ingresa el nombre del jugador {i + 1}: ");
+            nombres[i] = Console.ReadLine().ToUpper();
+            Console.Clear();
+            
+            Console.WriteLine($"{nombres[i]}, selecciona un color: ");
+            ImprimirColores(colors);
+            Console.Write("> ");
+
+            do {
+                try
+                {
+                    int index = Int32.Parse(Console.ReadLine());
+
+                    if(index < 1 || index > colors.Length)
+                        throw new Exception();
+
+                    colorJugadores[i] = colors[index-1];
+
+                    if(colorJugadores[0] == colorJugadores[1]) 
+                    {
+                        error = "Este color ya ha sido seleccionado";
+                        throw new Exception();
+                    }
+                    break;
+                }
+                catch (System.Exception)
+                {
+                    Console.SetCursorPosition(2, 3);
+                    Console.WriteLine(Spaces(10));
+                    Console.WriteLine(error);
+                    Console.SetCursorPosition(2, 3);
+                }
+
+            } while(true);
+        
+        }
 
         InicializarTablero(tablero);
 
@@ -48,11 +75,15 @@ class Program {
         
         int jugadorActual = 0;
 
+        Console.Clear();
         do {
-            char symbol = jugadorActual == 0 ? 'X' : 'O';
+            
+            Console.SetCursorPosition(0,0);
+            Console.Write(Spaces(Console.WindowWidth));
+            Console.SetCursorPosition(0,0);
 
-            ImprimirColor($"Turno de {nombres[jugadorActual]} ({symbol})\n", colorJugadores[jugadorActual]);
-            PrintTable(tablero, colorJugadores);
+            ImprimirColor($"Turno de {nombres[jugadorActual]} [{Jugador(jugadorActual)}]\n", colorJugadores[jugadorActual]);
+            ImprimirTablero(tablero, colorJugadores);
 
             PedirCoordenadas(tablero, jugadorActual, colorJugadores);
             jugadorActual = (jugadorActual+1) % 2;                                         
@@ -61,40 +92,64 @@ class Program {
 
         } while (!juegoTerminado);
 
-        PrintTable(tablero, colorJugadores);
-        int fichasJugador1 = ContarFichas(tablero, 1);
-        int fichasJugador2 = ContarFichas(tablero, 2);
+        int fichasJugador1 = ContarFichas(tablero, 0);
+        int fichasJugador2 = ContarFichas(tablero, 1);
 
-        Console.WriteLine($"Fin del juego! {nombres[0]}: {fichasJugador1} fichas, {nombres[1]}: {fichasJugador2} fichas.");
+        Console.SetCursorPosition(0,0);
+        Console.WriteLine(Spaces(Console.WindowWidth));
         if (fichasJugador1 > fichasJugador2)
         {
-            Console.WriteLine($"{nombres[0]} gana!");
+            ImprimirColor($"{nombres[0]} gana!\n", colorJugadores[0]);
         }
         else if (fichasJugador2 > fichasJugador1)
         {
-            Console.WriteLine($"{nombres[1]} gana!");
+            ImprimirColor($"{nombres[1]} gana!\n", colorJugadores[1]);
         }
         else
         {
             Console.WriteLine("Empate!");
         }
-    }
+        ImprimirTablero(tablero, colorJugadores);
 
-    static void InicializarTablero(string[,] tablero) {
-        tablero[3, 1] = "X";
-        tablero[3, 2] = "O";
-        tablero[3, 3] = "O";
-        tablero[3, 4] = "O";
-        tablero[3, 5] = "O";
-        tablero[3, 7] = "X";
-
-        tablero[0,3] = "X";
-        tablero[1,3] = "O";
-        tablero[2,3] = "O";
-
+        Console.WriteLine("\nFin del juego!");
+        ImprimirColor($"{nombres[0]}: {fichasJugador1} fichas.", colorJugadores[0]); 
+        ImprimirColor($"\n{nombres[1]}: {fichasJugador2} fichas.", colorJugadores[1]);
 
     }
-    static void PrintTable(string[,] tablero, ConsoleColor[] colorJugadores) {
+
+    static String Spaces(int cant)
+    {
+        String space = "";
+        for (int i = 0; i < cant; i++)
+        {
+            space += " ";
+        }
+        return space;
+    }
+
+    static void InicializarTablero(string[,] tablero) 
+    {
+        // tablero[3,3] = "X";
+        // tablero[3,4] = "O";
+        // tablero[4,3] = "O";
+        // tablero[4,4] = "X";
+
+        string[,] initialTablero = {
+            {"X", "X", "X", "X", "X", "X", "X", "X"},
+            {"X", "X", "X", "X", "X", "X", "X", "X"},
+            {"X", "X", "X", "X", "X", "X", "X", "X"},
+            {"O", "O", "O", "O", "O", "O", "O", null},
+            {"O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O"}
+        };
+        Array.Copy(initialTablero, tablero, initialTablero.Length);
+
+
+    }
+    static void ImprimirTablero(string[,] tablero, ConsoleColor[] colorJugadores) 
+    {
         
         Console.WriteLine("\n  A B C D E F G H");
 
@@ -125,38 +180,81 @@ class Program {
         Console.ResetColor();
     }
 
-    static void PrintColors(ConsoleColor[] colors) {
+    static void ImprimirColores(ConsoleColor[] colors) 
+    {
         int i = 1;
-        foreach (ConsoleColor color in colors) {
+        foreach (ConsoleColor color in colors) 
+        {
             Console.Write($"{i}.");
             ImprimirColor(color.ToString()+" ", color);
+
+            if(i==3) Console.WriteLine();
+
             i++;
         }
+
         Console.WriteLine();
     }
-    static void PedirCoordenadas(string[,] tablero, int jugador, ConsoleColor[] colorJugadores) {
-        Console.Write("Ingrese la fila-columna (a1): ");
-        String pos = Console.ReadLine().ToUpper();
+    
 
-        int columna = pos[0]-65;
-        int fila = Int32.Parse(pos[1].ToString())-1;
+    static void PedirCoordenadas(string[,] tablero, int jugador, ConsoleColor[] colorJugadores) 
+    {
+        int columna = 0, fila = 0;
+        String pos = "";
+        String msg = "";
 
-        if (EsMovimientoValido(tablero, fila, columna)) {
-            ColocarFicha(tablero, fila, columna, jugador, colorJugadores);
-            Reversi(tablero, fila, columna);
-        } else {
-            Console.WriteLine("Movimiento inválido... Intente nuevamente.");
-            PedirCoordenadas(tablero, jugador, colorJugadores);
-        }
+        Console.SetCursorPosition(0,12);
+        Console.Write("> ");
+
+
+        do
+        {
+
+            Console.SetCursorPosition(2,12);
+            Console.Write(Spaces(pos.Length));
+            Console.SetCursorPosition(2,12);
+            Console.ForegroundColor = colorJugadores[jugador];
+            pos = Console.ReadLine().ToUpper();
+            
+            Console.ResetColor();
+            try
+            {            
+                columna = pos[0]-65;
+                fila = Int32.Parse(pos[1].ToString())-1;
+
+                if (!EsMovimientoValido(tablero, fila, columna)) {
+                    throw new Exception();
+                }
+            }
+            catch (System.Exception)
+            {
+                Console.SetCursorPosition(25,6);
+                msg = "Movimiento inválido... Intente nuevamente.";
+                Console.Write(msg);
+                Console.SetCursorPosition(0,12);
+                continue;
+            }
+            
+            ColocarFicha(tablero, fila, columna, jugador);
+            Reversi(tablero, fila, columna);    
+            
+            
+            Console.SetCursorPosition(25,6);
+            Console.Write(Spaces(msg.Length));
+            Console.SetCursorPosition(0,12);
+            Console.Write(Spaces(10));
+            Console.SetCursorPosition(0,12);
+            break;
+        } while (true);
     }
     static bool EsMovimientoValido(string[,] tablero, int fila, int columna) {
         if(!(fila >= 0 && fila <=7) || !(columna >= 0 && columna <=7))
             return false;
 
-    return tablero[fila,columna] == null;
+        return tablero[fila,columna] == null;
     }
 
-    static void ColocarFicha(string[,] tablero, int fila, int columna, int jugador, ConsoleColor[] colorJugadores) {
+    static void ColocarFicha(string[,] tablero, int fila, int columna, int jugador) {
         String[] simbolos = ["X","O"];
         tablero[fila,columna] = simbolos[jugador];
     }
@@ -185,43 +283,41 @@ class Program {
 
     static void Reversi(string[,] tablero, int ultimaFila, int ultimaColumna)
     {
-        string[,] t = tablero;
-        int f = ultimaFila;
-        int c = ultimaColumna;
+        string[,] tab = tablero;
+        int fil = ultimaFila;
+        int col = ultimaColumna;
 
-        string jugador = t[f,c];
+        string jugador = tab[fil,col];
 
         //up, down, left right
         string[] direcciones = ["u", "d", "l", "r"];
         
         foreach (string dir in direcciones)
-        {
-            EncontrarPareja(t,jugador,f, c, dir);
-
-        }
+            EncontrarPareja(tab,jugador,fil, col, dir);
+        
 
     }
 
-    static Boolean EncontrarPareja(String[,] tablero, String jugador, int f, int c, String direccion)
+    static Boolean EncontrarPareja(String[,] tab, String jug, int fila, int col, String dir)
     {
         int x = 0, y = 0;
 
-        switch (direccion.ToLower())
+        switch (dir.ToLower())
         {
             case "u": y=-1; break;
             case "d": y=1; break;
             case "l": x=-1; break;
             case "r": x=1; break;
             default: 
-                Console.WriteLine("Error direccion:"+direccion); 
+                Console.WriteLine("Error direccion:"+dir); 
                 break;
         }
 
-        String siguienteCasilla;
-        String jugadorReversa = JugadorReversa(jugador);
+        String sigCasilla;
+        String jugReversa = JugadorReversa(jug);
         try
         {
-            siguienteCasilla = tablero[f+y,c+x];
+            sigCasilla = tab[fila+y,col+x];
             
         }
         catch (System.Exception)
@@ -230,17 +326,17 @@ class Program {
         }
 
         // o jugador, o nulo
-        if(siguienteCasilla == null)
+        if(sigCasilla == null)
             return false;
 
-        if(siguienteCasilla == jugadorReversa)
-            if(EncontrarPareja(tablero, jugador, f+y, c+x, direccion)){
-                tablero[f+y,c+x] = jugador;
-                Reversi(tablero, f+y, c+x);
+        if(sigCasilla == jugReversa)
+            if(EncontrarPareja(tab, jug, fila+y, col+x, dir)){
+                tab[fila+y,col+x] = jug;
+                Reversi(tab, fila+y, col+x);
                 return true;
             }
         // 
-        return jugador == siguienteCasilla;
+        return jug == sigCasilla;
 
     }
 
